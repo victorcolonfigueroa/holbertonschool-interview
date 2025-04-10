7dee56c0e370f987302b2b2565b7c1c82419563e
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2025-04-10 10:01:59 -0400

    refactor: clean up whitespace in lockboxes script

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,24 +1,14 @@
- #!/usr/bin/python3
-
+#!/usr/bin/python3
 def canUnlockAll(boxes):
-
     n= len(boxes)
     visited = set()
     queue = []
-
-
     queue.append(0)
     visited.add(0)
-
-
     while queue:
         current_box = queue.pop(0)
-
-
         for key in boxes[current_box]:
             if key < n and key not in visited:
                 visited.add(key)
                 queue.append(key)
-
-
     return len(visited) == n
